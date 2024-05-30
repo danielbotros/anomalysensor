f24bd28969579d524226bc509f5fe73fd1f86574
danielbotros <daniel.botros@viam.com>
2024-05-30 16:37:26 -0400

    Update script and gitignore venv/

--- run.sh
+++ run.sh
@@ -1,20 +1,42 @@
-#!/bin/bash
+#!/bin/sh
 cd `dirname $0`
 
-if [ -f .installed ]
-  then
-    source viam-env/bin/activate
-  else
-    python3 -m pip install --user virtualenv --break-system-packages
-    python3 -m venv viam-env
-    source viam-env/bin/activate
-    pip3 install --upgrade -r requirements.txt
-    if [ $? -eq 0 ]
-      then
-        touch .installed
+# Create a virtual environment to run our code
+VENV_NAME="venv"
+PYTHON="$VENV_NAME/bin/python"
+ENV_ERROR="This module requires Python >=3.8, pip, and virtualenv to be installed."
+
+if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
+    echo "Failed to create virtualenv."
+    if command -v apt-get >/dev/null; then
+        echo "Detected Debian/Ubuntu, attempting to install python3-venv automatically."
+        SUDO="sudo"
+        if ! command -v $SUDO >/dev/null; then
+            SUDO=""
+        fi
+		if ! apt info python3-venv >/dev/null 2>&1; then
+			echo "Package info not found, trying apt update"
+			$SUDO apt -qq update >/dev/null
+		fi
+        $SUDO apt install -qqy python3-venv >/dev/null 2>&1
+        if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
+            echo $ENV_ERROR >&2
+            exit 1
+        fi
+    else
+        echo $ENV_ERROR >&2
+        exit 1
     fi
+fi
+
+# remove -U if viam-sdk should not be upgraded whenever possible
+# -qq suppresses extraneous output from pip
+echo "Virtualenv found/created. Installing/upgrading Python packages..."
+if ! $PYTHON -m pip install -r requirements.txt -Uqq; then
+    exit 1
 fi
 
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
-exec python3 -m src $@
+echo "Starting module..."
+exec $PYTHON -m src $@
